d0a0f6a0236c17418d487266a099e14923d38ecb
Abhishek-90 <holaniabhishek90@gmail.com>
2022-11-16 19:50:48 +0530

    minor change in route components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -8,9 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-        </Routes>
-        <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<><Header/><Home/></>}/>
         </Routes>
       </Router>
     </div>
