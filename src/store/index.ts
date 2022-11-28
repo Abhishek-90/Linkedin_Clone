a4a7f90f3698fbaa325a40ae6d63a3f0005b82a2
Abhishek-90 <holaniabhishek90@gmail.com>
2022-11-28 14:47:14 +0530

    applied middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
-
+import thunkMiddleware  from "redux-thunk";
 import reducers from "../reducers/index";
 
-const store = createStore(reducers, {});
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 export default store;
