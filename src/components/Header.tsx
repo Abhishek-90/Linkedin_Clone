b82e988bb37ee21090004f7165e5fcd518ad2649
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-02 17:32:13 +0530

    clean up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { signOutAPI } from "../actions";
 
 function Header(props: any) {
-  console.log(props.user?.photoURL);
   return (
     <Container>
       <Content>
