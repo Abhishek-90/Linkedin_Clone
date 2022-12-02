efccf085960b922f048db84cc8a36da89cb22981
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-02 15:38:04 +0530

    use effect added

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,41 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import { useEffect } from "react";
+import { getUserAuth } from "./actions/index";
+import { connect } from "react-redux";
 
-function App() {
+function App(props: any) {
+  useEffect(() => {
+    props.getUserAuth();
+  }, []);
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<><Header/><Home/></>}/>
+          <Route
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state: any) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch: any) => ({
+  getUserAuth: () => dispatch(getUserAuth()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
