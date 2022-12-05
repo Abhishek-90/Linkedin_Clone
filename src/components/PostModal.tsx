d95f4bb77ad4e2bfd7f6a250a9877db8e199fcb5
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-05 15:47:03 +0530

    action buttons in modal added

--- src/components/PostModal.tsx
+++ src/components/PostModal.tsx
@@ -27,7 +27,45 @@ function PostModal(props: any) {
             </div>
           </UserInfo>
         </SharedContent>
-        <SharedActions>Actions</SharedActions>
+        <SharedCreations>
+          <AttachAssets>
+            <AssetButton>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill="currentColor"
+                className="mercado-match"
+                width="24"
+                height="24"
+                focusable="false"
+              >
+                <path d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm1 13a1 1 0 01-.29.71L16 14l-2 2-6-6-4 4V7a1 1 0 011-1h14a1 1 0 011 1zm-2-7a2 2 0 11-2-2 2 2 0 012 2z"></path>
+              </svg>
+            </AssetButton>
+            <AssetButton>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill="currentColor"
+                className="mercado-match"
+                width="24"
+                height="24"
+                focusable="false"
+              >
+                <path d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm-9 12V8l6 4z"></path>
+              </svg>
+            </AssetButton>
+          </AttachAssets>
+          <SharedComment>
+            <AssetButton>
+              <img src="/images/post-comment.svg" alt="" />
+              <span>Anyone</span>
+            </AssetButton>
+          </SharedComment>
+          <PostButton>Post</PostButton>
+        </SharedCreations>
       </Content>
     </Container>
   );
@@ -124,7 +162,70 @@ const UserInfo = styled.div`
   }
 `;
 
-const SharedActions = styled.div``;
+const SharedCreations = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 24px 12px 16px;
+`;
+
+const AssetButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  transition-duration: 167ms;
+  height: 40px;
+  min-width: auto;
+  border-radius: 50%;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+
+  svg {
+    color: rgba(0, 0, 0, 0.6);
+  }
+`;
+
+const AttachAssets = styled.div`
+  display: flex;
+  padding-right: 8px;
+  align-items: center;
+  border-right: 1px solid rgba(0, 0, 0, 0.15);
+
+  ${AssetButton} {
+    width: 40px;
+  }
+`;
+
+const SharedComment = styled.div`
+  margin-right: auto;
+  padding-left: 8px;
+
+  ${AssetButton} {
+    svg,
+    img {
+      margin-right: 5px;
+    }
+  }
+`;
+
+const PostButton = styled.button`
+  min-width: 60px;
+  border-radius: 20px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background: #0a66c2;
+  color: white;
+  border: none;
+  outline: none;
+
+  &:hover {
+    background: #004182;
+  }
+`;
 
 const mapStateToProps = (state: any) => {
   return {
