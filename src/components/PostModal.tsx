7cbe75f066dd6330e55a6f49d7906799f0ca97f0
Abhishek-90 <holaniabhishek90@gmail.com>
2022-12-08 16:25:45 +0530

    minor change

--- src/components/PostModal.tsx
+++ src/components/PostModal.tsx
@@ -122,7 +122,7 @@ function PostModal(props: any) {
             </SharedContent>
             <SharedCreations>
               <AttachAssets>
-                <AssetButton onClick={() => setAssetArea("image")}>
+                <AssetButton onClick={() => switchAssetArea("image")}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
@@ -136,7 +136,7 @@ function PostModal(props: any) {
                     <path d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm1 13a1 1 0 01-.29.71L16 14l-2 2-6-6-4 4V7a1 1 0 011-1h14a1 1 0 011 1zm-2-7a2 2 0 11-2-2 2 2 0 012 2z"></path>
                   </svg>
                 </AssetButton>
-                <AssetButton onClick={() => setAssetArea("video")}>
+                <AssetButton onClick={() => switchAssetArea("video")}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
